fbdbae893fc160ef989e87fa6a5a441cec0bbc01
evgsavelov <savelov08@ya.ru>
2020-05-13 11:54:35 +0300

    java

---
+++ Miles copy.java
@@ -0,0 +1,7 @@
+public class Miles {
+    public static void main(String[] args) {
+        int a = 6000; //cost of ticket
+        int b = a/20; // travel miles
+        System.out.println(b);
+    }
+}
