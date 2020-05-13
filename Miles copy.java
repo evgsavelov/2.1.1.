0b02198fcf25c36cb0f02c93022c3225a50134b9
evgsavelov <savelov08@ya.ru>
2020-05-13 13:04:13 +0300

    correct_2

--- Miles copy.java
+++
@@ -1,7 +0,0 @@
-public class Miles {
-    public static void main(String[] args) {
-        int a = 6000; //cost of ticket
-        int b = a/20; // travel miles
-        System.out.println(b);
-    }
-}
