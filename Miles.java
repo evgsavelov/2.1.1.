19bd8fe268e7c96dd348a6ca6ea9df425a4dcb1f
evgsavelov <savelov08@ya.ru>
2020-05-14 13:08:55 +0300

    correct_3

--- Miles.java
+++ Miles.java
@@ -1,7 +1,7 @@
 public class Miles {
     public static void main(String[] args) {
         int cost = 6000; //cost of ticket
-        int miles = cost/20; // travel miles
+        int miles = cost / 20; // travel miles
         System.out.println(miles);
     }
 }
